78bf70058c81f8db5781a77349367d75fa9c7759
Derek Poe <48400713+Derek-Poe@users.noreply.github.com>
2019-07-30 21:47:45 +0900

    Update Scraper.py

--- Scraper.py
+++ Scraper.py
@@ -6,6 +6,7 @@ reqHeaders = {"Accept": "*/*","Accept-Encoding": "gzip, deflate, br","Accept-Lan
 
 zillowRequest = requests.Session()
 zillowURL = "https://www.zillow.com/beauregard-parish-la/fsbo/?searchQueryState={%22pagination%22:{},%22mapBounds%22:{%22west%22:-94.08709973828121,%22east%22:-92.63141126171871,%22south%22:29.738740300239186,%22north%22:31.539331063455272},%22usersSearchTerm%22:%22Beauregard%20Parish%20LA%22,%22regionSelection%22:[{%22regionId%22:2886,%22regionType%22:4}],%22isMapVisible%22:true,%22mapZoom%22:9,%22filterState%22:{%22isPreMarketForeclosure%22:{%22value%22:false},%22isPreMarketPreForeclosure%22:{%22value%22:false},%22isMakeMeMove%22:{%22value%22:false},%22isForSaleByAgent%22:{%22value%22:false},%22isAuction%22:{%22value%22:false},%22isNewConstruction%22:{%22value%22:false},%22isForSaleForeclosure%22:{%22value%22:false},%22isComingSoon%22:{%22value%22:false}},%22isListVisible%22:true}"
+#zillowURL = "https://www.zillow.com/homes/for_sale/?searchQueryState={%22pagination%22:{},%22mapBounds%22:{%22west%22:-93.65082257371114,%22east%22:-92.95181744675801,%22south%22:30.652397782528343,%22north%22:31.07617909906113},%22isMapVisible%22:true,%22filterState%22:{%22sortSelection%22:{%22value%22:%22days%22},%22isForRent%22:{%22value%22:true}},%22isListVisible%22:true,%22mapZoom%22:11}"
 zillowSession = zillowRequest.get(zillowURL, headers=reqHeaders)
 zillowContent = zillowSession.content
 zillowSoup = soup(zillowContent, "html.parser")
@@ -39,21 +40,30 @@ for x in photoTiles:
 # - parking *
 # - lot *
 # - priceSqft *
-# - flooring
-# - appliances
-# - stories
-# - exterior
-# - parcelNumber
-# - zoning
-# - roof
-# - newConstruction
-# - sunScore
-# - region
-# - hasHOAFee
+# - flooring *
+# - appliances *
+# - parkingFeatures *
+# - hasGarage *
+# - stories *
+# - exteriorFeatures *
+# - parcelNumber *
+# - newConstruction *
+# - sewerInformation *
+# - sunscore ########
+# - region *
+# - hasHOAFee *
 # - taxAssessedValue
 # - annualTaxAmount
-# - MLSID
 # - school
+# - construction
+# - roof
+# - exterior
+# - floor
+# - walls
+# - miscellaneous
+# - style
+# - water
+# - fence
 # - priceHistory
 # - taxHistory
 # - rentZestimate
@@ -65,8 +75,8 @@ y = 0
 for x in houseLinks:
   print("#########################")
   #----------------------------------------------------------------------------------------------------------------
-  #zillowURL = "https://www.zillow.com/homedetails/164-Kidder-Loop-Merryville-LA-70653/106012997_zpid/"
-  zillowURL = x
+  zillowURL = "https://www.zillow.com/homedetails/164-Kidder-Loop-Merryville-LA-70653/106012997_zpid/"
+  #zillowURL = x
   #----------------------------------------------------------------------------------------------------------------
   zillowSession = zillowRequest.get(zillowURL, headers=reqHeaders)
   zillowContent = zillowSession.content
@@ -201,7 +211,174 @@ for x in houseLinks:
   except IndexError:
     houses[y].priceSqft = "unavailable"
   #
-  
+  try:
+    flooringContainer = houses[y].findAll("div",{"class":"ds-home-facts-and-features reso-facts-features"})[0].findAll("td",string="Flooring")[0].parent.findAll("span")
+    flooring = flooringContainer[0].text
+    houses[y].flooring = flooring
+  except IndexError:
+    houses[y].flooring = "unavailable"
+  #
+  try:
+    appliancesContainer = houses[y].findAll("div",{"class":"ds-home-facts-and-features reso-facts-features"})[0].findAll("td",string="Appliances included in sale")[0].parent.findAll("span")
+    appliances = appliancesContainer[0].text
+    houses[y].appliances = appliances
+  except IndexError:
+    houses[y].appliances = "unavailable"
+  #
+  try:
+    parkingFeaturesContainer = houses[y].findAll("div",{"class":"ds-home-facts-and-features reso-facts-features"})[0].findAll("td",string="Parking features")[0].parent.findAll("div")
+    parkingFeatures = parkingFeaturesContainer[0].text
+    houses[y].parkingFeatures = parkingFeatures
+  except IndexError:
+    houses[y].parkingFeatures = "unavailable"
+  #
+  try:
+    hasGarageContainer = houses[y].findAll("div",{"class":"ds-home-facts-and-features reso-facts-features"})[0].findAll("td",string="Has garage")[0].parent.findAll("span")
+    hasGarage = hasGarageContainer[0].text
+    houses[y].hasGarage = hasGarage
+  except IndexError:
+    houses[y].hasGarage = "unavailable"
+  #
+  try:
+    storiesContainer = houses[y].findAll("div",{"class":"ds-home-facts-and-features reso-facts-features"})[0].findAll("td",string="Stories")[0].parent.findAll("span")
+    stories = storiesContainer[0].text
+    houses[y].stories = stories
+  except IndexError:
+    houses[y].stories = "unavailable"
+  #
+  try:
+    exteriorFeaturesContainer = houses[y].findAll("div",{"class":"ds-home-facts-and-features reso-facts-features"})[0].findAll("td",string="Exterior features")[0].parent.findAll("div")
+    exteriorFeatures = exteriorFeaturesContainer[0].text
+    houses[y].exteriorFeatures = exteriorFeatures
+  except IndexError:
+    houses[y].exteriorFeatures = "unavailable"
+  #
+  try:
+    parcelNumberContainer = houses[y].findAll("div",{"class":"ds-home-facts-and-features reso-facts-features"})[0].findAll("td",string="Parcel number")[0].parent.findAll("span")
+    parcelNumber = parcelNumberContainer[0].text
+    houses[y].parcelNumber = parcelNumber
+  except IndexError:
+    houses[y].parcelNumber = "unavailable"
+  #
+  try:
+    newConstructionContainer = houses[y].findAll("div",{"class":"ds-home-facts-and-features reso-facts-features"})[0].findAll("td",string="New construction")[0].parent.findAll("span")
+    newConstruction = newConstructionContainer[0].text
+    houses[y].newConstruction = newConstruction
+  except IndexError:
+    houses[y].newConstruction = "unavailable"
+  #
+  try:
+    sewerInformationContainer = houses[y].findAll("div",{"class":"ds-home-facts-and-features reso-facts-features"})[0].findAll("td",string="Sewer information")[0].parent.findAll("span")
+    sewerInformation = sewerInformationContainer[0].text
+    houses[y].sewerInformation = sewerInformation
+  except IndexError:
+    houses[y].sewerInformation = "unavailable"
+  #
+  #try:
+    #sunscoreContainer = houses[y].findAll("div",{"class":"ds-home-facts-and-features reso-facts-features"})[0].findAll("td",string="Sunscore")[0].parent.findAll("div")
+    #sunscore = sunscoreContainer[0].text
+    #houses[y].sunscore = sunscore
+  #except IndexError:
+    #houses[y].sunscore = "unavailable"
+  #
+  try:
+    regionContainer = houses[y].findAll("div",{"class":"ds-home-facts-and-features reso-facts-features"})[0].findAll("td",string="Region")[0].parent.findAll("span")
+    region = regionContainer[0].text
+    houses[y].region = region
+  except IndexError:
+    houses[y].region = "unavailable"
+  #
+  try:
+    hasHOAFeeContainer = houses[y].findAll("div",{"class":"ds-home-facts-and-features reso-facts-features"})[0].findAll("td",string="Has HOA fee")[0].parent.findAll("span")
+    hasHOAFee = hasHOAFeeContainer[0].text
+    houses[y].hasHOAFee = hasHOAFee
+  except IndexError:
+    houses[y].hasHOAFee = "unavailable"
+  #
+  try:
+    taxAssessedValueContainer = houses[y].findAll("div",{"class":"ds-home-facts-and-features reso-facts-features"})[0].findAll("td",string="Tax assessed value")[0].parent.findAll("span")
+    taxAssessedValue = taxAssessedValueContainer[0].text
+    houses[y].taxAssessedValue = taxAssessedValue
+  except IndexError:
+    houses[y].taxAssessedValue = "unavailable"
+  #
+  try:
+    annualTaxAmountContainer = houses[y].findAll("div",{"class":"ds-home-facts-and-features reso-facts-features"})[0].findAll("td",string="Annual tax amount")[0].parent.findAll("span")
+    annualTaxAmount = annualTaxAmountContainer[0].text
+    houses[y].annualTaxAmount = annualTaxAmount
+  except IndexError:
+    houses[y].annualTaxAmount = "unavailable"
+  #
+  try:
+    schoolContainer = houses[y].findAll("div",{"class":"ds-home-facts-and-features reso-facts-features"})[0].findAll("td",string="School")[0].parent.findAll("span")
+    school = schoolContainer[0].text
+    houses[y].school = school
+  except IndexError:
+    houses[y].school = "unavailable"
+  #
+  try:
+    constructionContainer = houses[y].findAll("div",{"class":"ds-home-facts-and-features reso-facts-features"})[0].findAll("td",string="Construction")[0].parent.findAll("span")
+    construction = constructionContainer[0].text
+    houses[y].construction = construction
+  except IndexError:
+    houses[y].construction = "unavailable"
+  #
+  try:
+    roofContainer = houses[y].findAll("div",{"class":"ds-home-facts-and-features reso-facts-features"})[0].findAll("td",string="Roof")[0].parent.findAll("span")
+    roof = roofContainer[0].text
+    houses[y].roof = roof
+  except IndexError:
+    houses[y].roof = "unavailable"
+  #
+  try:
+    exteriorContainer = houses[y].findAll("div",{"class":"ds-home-facts-and-features reso-facts-features"})[0].findAll("td",string="Exterior")[0].parent.findAll("span")
+    exterior = exteriorContainer[0].text
+    houses[y].exterior = exterior
+  except IndexError:
+    houses[y].exterior = "unavailable"
+  #
+  try:
+    floorContainer = houses[y].findAll("div",{"class":"ds-home-facts-and-features reso-facts-features"})[0].findAll("td",string="Floor")[0].parent.findAll("span")
+    floor = floorContainer[0].text
+    houses[y].floor = floor
+  except IndexError:
+    houses[y].floor = "unavailable"
+  #
+  try:
+    wallsContainer = houses[y].findAll("div",{"class":"ds-home-facts-and-features reso-facts-features"})[0].findAll("td",string="Walls")[0].parent.findAll("span")
+    walls = wallsContainer[0].text
+    houses[y].walls = walls
+  except IndexError:
+    houses[y].walls = "unavailable"
+  #
+  try:
+    miscellaneousContainer = houses[y].findAll("div",{"class":"ds-home-facts-and-features reso-facts-features"})[0].findAll("td",string="Miscellaneous")[0].parent.findAll("span")
+    miscellaneous = miscellaneousContainer[0].text
+    houses[y].miscellaneous = miscellaneous
+  except IndexError:
+    houses[y].miscellaneous = "unavailable"
+  #
+  try:
+    styleContainer = houses[y].findAll("div",{"class":"ds-home-facts-and-features reso-facts-features"})[0].findAll("td",string="Style")[0].parent.findAll("span")
+    style = styleContainer[0].text
+    houses[y].style = style
+  except IndexError:
+    houses[y].style = "unavailable"
+  #
+  try:
+    waterContainer = houses[y].findAll("div",{"class":"ds-home-facts-and-features reso-facts-features"})[0].findAll("td",string="Water")[0].parent.findAll("span")
+    water = waterContainer[0].text
+    houses[y].water = water
+  except IndexError:
+    houses[y].water = "unavailable"
+  #
+  try:
+    fenceContainer = houses[y].findAll("div",{"class":"ds-home-facts-and-features reso-facts-features"})[0].findAll("td",string="Miscellaneous: Fence (Cyc/Wd)")[0].parent.findAll("span")
+    fence = fenceContainer[0].text
+    houses[y].fence = fence
+  except IndexError:
+    houses[y].fence = "unavailable"
+  #
   
   print("Address: " + houses[y].address)
   print("Price: " + houses[y].price)
@@ -221,10 +398,34 @@ for x in houseLinks:
   print("Parking: " + houses[y].parking)
   print("Lot: " + houses[y].lot)
   print("Price/SqFt: " + houses[y].priceSqft)
+  print("Flooring: " + houses[y].flooring)
+  print("Appliances: " + houses[y].appliances)
+  print("Parking Features: " + houses[y].parkingFeatures)
+  print("Has Garage: " + houses[y].hasGarage)
+  print("Stories: " + houses[y].stories)
+  print("Exterior Features: " + houses[y].exteriorFeatures)
+  print("Parcel Number: " + houses[y].parcelNumber)
+  print("New Construction: " + houses[y].newConstruction)
+  print("Sewer Information: " + houses[y].sewerInformation)
+  #print("Sunscore: " + houses[y].sunscore)
+  print("Region: " + houses[y].region)
+  print("Has HOA Fee: " + houses[y].hasHOAFee)
+  print("Tax Assessed Value: " + houses[y].taxAssessedValue)
+  print("Annual Tax Amount: " + houses[y].annualTaxAmount)
+  print("School: " + houses[y].school)
+  print("Construction: " + houses[y].construction)
+  print("Roof: " + houses[y].roof)
+  print("Exterior: " + houses[y].exterior)
+  print("Floor: " + houses[y].floor)
+  print("Walls: " + houses[y].walls)
+  print("Miscellaneous: " + houses[y].miscellaneous)
+  print("Style: " + houses[y].style)
+  print("Water: " + houses[y].water)
+  print("Fence: " + houses[y].fence)
   
   print("#########################")
-  y += 1
-  #break
+  #y += 1
+  break
   
 
   
