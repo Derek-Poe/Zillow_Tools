b425b7ba47a0646a8720f60fc9e7ef14044f3b1f
Derek Poe <48400713+Derek-Poe@users.noreply.github.com>
2019-07-30 20:12:19 +0900

    Update Scraper.py

--- Scraper.py
+++ Scraper.py
@@ -23,22 +23,22 @@ for x in photoTiles:
 #House Info#
 # - address *
 # - price *
-# - beds
-# - baths
-# - fullBaths
-# - halfBaths
-# - sqft
-# - zestimate
-# - timeOnZillow
-# - views
-# - saves
-# - type
-# - yearBuilt
-# - heating
-# - cooling
-# - parking
-# - lot
-# - priceSqft
+# - beds *
+# - baths *
+# - fullBaths *
+# - halfBaths *
+# - sqft *
+# - zestimate #####################
+# - timeOnZillow *
+# - views *
+# - saves *
+# - type *
+# - yearBuilt *
+# - heating *
+# - cooling *
+# - parking *
+# - lot *
+# - priceSqft *
 # - flooring
 # - appliances
 # - stories
@@ -63,7 +63,11 @@ for x in photoTiles:
 houses = []
 y = 0
 for x in houseLinks:
+  print("#########################")
+  #----------------------------------------------------------------------------------------------------------------
+  #zillowURL = "https://www.zillow.com/homedetails/164-Kidder-Loop-Merryville-LA-70653/106012997_zpid/"
   zillowURL = x
+  #----------------------------------------------------------------------------------------------------------------
   zillowSession = zillowRequest.get(zillowURL, headers=reqHeaders)
   zillowContent = zillowSession.content
   zillowSoup = soup(zillowContent, "html.parser")
@@ -71,19 +75,156 @@ for x in houseLinks:
   
   houses.append(zillowSoup)
   
-  addressContainer = houses[y].findAll("h1",{"class":"ds-address-container"})[0].findAll("span")
-  address = addressContainer[0].text + addressContainer[1].text
-  houses[y].address = address
+  try:
+    addressContainer = houses[y].findAll("h1",{"class":"ds-address-container"})[0].findAll("span")
+    address = addressContainer[0].text + addressContainer[1].text
+    houses[y].address = address
+  except IndexError:
+    houses[y].address = "unavailable"
   #
-  priceContainer = houses[y].findAll("span",{"class":"ds-value"})
-  price = priceContainer[0].text
-  houses[y].price = price
+  try:
+    priceContainer = houses[y].findAll("span",{"class":"ds-value"})
+    price = priceContainer[0].text
+    houses[y].price = price
+  except IndexError:
+    houses[y].price = "unavailable"
+  #
+  try:
+    bedsContainer = houses[y].findAll("div",{"class":"ds-home-facts-and-features reso-facts-features"})[0].findAll("td",string="Bedrooms")[0].parent.findAll("span")
+    beds = bedsContainer[0].text
+    houses[y].beds = beds
+  except IndexError:
+    houses[y].beds = "unavailable"
+  #
+  try:
+    bathsContainer = houses[y].findAll("div",{"class":"ds-home-facts-and-features reso-facts-features"})[0].findAll("td",string="Bathrooms")[0].parent.findAll("span")
+    baths = bathsContainer[0].text
+    houses[y].baths = baths
+  except IndexError:
+    houses[y].baths = "unavailable"
+  #
+  try:
+    fullBathsContainer = houses[y].findAll("div",{"class":"ds-home-facts-and-features reso-facts-features"})[0].findAll("td",string="Full bathrooms")[0].parent.findAll("span")
+    fullBaths = fullBathsContainer[0].text
+    houses[y].fullBaths = fullBaths
+  except IndexError:
+    houses[y].fullBaths = "unavailable"
+  #
+  try:
+    halfBathsContainer = houses[y].findAll("div",{"class":"ds-home-facts-and-features reso-facts-features"})[0].findAll("td",string="1/2 bathrooms")[0].parent.findAll("span")
+    halfBaths = halfBathsContainer[0].text
+    houses[y].halfBaths = halfBaths
+  except IndexError:
+    houses[y].halfBaths = "unavailable"
+  #
+  try:
+    sqftContainer = houses[y].findAll("div",{"class":"ds-home-facts-and-features reso-facts-features"})[0].findAll("td",string="Total interior livable area")[0].parent.findAll("span")
+    sqft = sqftContainer[0].text
+    houses[y].sqft = sqft
+  except IndexError:
+    houses[y].sqft = "unavailable"
+  #
+  #try:
+    #zestimateContainer = houses[y].findAll("p",{"class":"Text-sc-1vuq29o-0 sc-1w7ie4j-1 sc-111s858-8 fFiQLD"})
+    #zestimate = zestimateContainer[0].text
+    #houses[y].zestimate = zestimate
+  #except IndexError:
+    #houses[y].zestimate = "unavailable"
+  #
+  try:
+    timeOnZillowContainer = houses[y].findAll("div",{"class":"ds-overview-stat-value"})
+    timeOnZillow = timeOnZillowContainer[0].text
+    houses[y].timeOnZillow = timeOnZillow
+  except IndexError:
+    houses[y].timeOnZillow = "unavailable"
+  #
+  try:
+    viewsContainer = houses[y].findAll("div",{"class":"ds-overview-stat-value"})
+    views = viewsContainer[1].text
+    houses[y].views = views
+  except IndexError:
+    houses[y].views = "unavailable"
+  #
+  try:
+    savesContainer = houses[y].findAll("div",{"class":"ds-overview-stat-value"})
+    saves = savesContainer[2].text
+    houses[y].saves = saves
+  except IndexError:
+    houses[y].saves = "unavailable"
+  #
+  try:
+    typeContainer = houses[y].findAll("span",{"class":"ds-body ds-home-fact-value"})
+    type = typeContainer[0].text
+    houses[y].type = type
+  except IndexError:
+    houses[y].type = "unavailable"
+  #
+  try:
+    yearBuiltContainer = houses[y].findAll("span",{"class":"ds-body ds-home-fact-value"})
+    yearBuilt = yearBuiltContainer[1].text
+    houses[y].yearBuilt = yearBuilt
+  except IndexError:
+    houses[y].yearBuilt = "unavailable"
+  #
+  try:
+    heatingContainer = houses[y].findAll("span",{"class":"ds-body ds-home-fact-value"})
+    heating = heatingContainer[2].text
+    houses[y].heating = heating
+  except IndexError:
+    houses[y].heating = "unavailable"
+  #
+  try:
+    coolingContainer = houses[y].findAll("span",{"class":"ds-body ds-home-fact-value"})
+    cooling = coolingContainer[3].text
+    houses[y].cooling = cooling
+  except IndexError:
+    houses[y].cooling = "unavailable"
+  #
+  try:
+    parkingContainer = houses[y].findAll("span",{"class":"ds-body ds-home-fact-value"})
+    parking = parkingContainer[4].text
+    houses[y].parking = parking
+  except IndexError:
+    houses[y].parking = "unavailable"
+  #
+  try:
+    lotContainer = houses[y].findAll("span",{"class":"ds-body ds-home-fact-value"})
+    lot = lotContainer[5].text
+    houses[y].lot = lot
+  except IndexError:
+    houses[y].lot = "unavailable"
+  #
+  try:
+    priceSqftContainer = houses[y].findAll("span",{"class":"ds-body ds-home-fact-value"})
+    priceSqft = priceSqftContainer[6].text
+    houses[y].priceSqft = priceSqft
+  except IndexError:
+    houses[y].priceSqft = "unavailable"
   #
   
-  print(houses[y].address)
-  print(houses[y].price)
+  
+  print("Address: " + houses[y].address)
+  print("Price: " + houses[y].price)
+  print("Beds: " + houses[y].beds)
+  print("Baths: " + houses[y].baths)
+  print("Full Baths: " + houses[y].fullBaths)
+  print("Half Baths: " + houses[y].halfBaths)
+  print("Sqft: " + houses[y].sqft)
+  #print("Zestimate: " + houses[y].zestimate)
+  print("Time on Zillow: " + houses[y].timeOnZillow)
+  print("Views: " + houses[y].views)
+  print("Saves: " + houses[y].saves)
+  print("Type: " + houses[y].type)
+  print("Year Built: " + houses[y].yearBuilt)
+  print("Heating: " + houses[y].heating)
+  print("Cooling: " + houses[y].cooling)
+  print("Parking: " + houses[y].parking)
+  print("Lot: " + houses[y].lot)
+  print("Price/SqFt: " + houses[y].priceSqft)
+  
+  print("#########################")
   y += 1
-  break
+  #break
   
 
   
